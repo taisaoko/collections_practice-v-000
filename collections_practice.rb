4c35cdb9c9f5fc5fba25c248d5bfab43d4666f19
taisaoko <ttd3calvin@gmail.com>
2018-08-25 15:41:32 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,6 +19,7 @@ def swap_elements_from_to(array, index, destination_index)
   element = array[index]  
   array[index] = array[destination_index]
   array[destination_index] = element
+  array
 end
 
 def reverse_array(array)
